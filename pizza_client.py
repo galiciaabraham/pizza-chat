05b47918b0be5b5fb93ccb63b980363a846d8708
galiciaabraham <galicia.abraham@hotmail.com>
2025-06-06 07:09:50 -0600

    Implemented Tkinter GUI for pizza ordering and send order as JSON

--- pizza_client.py
+++ pizza_client.py
@@ -1,11 +1,62 @@
 import socket
+from tkinter import *
+from tkinter import messagebox
+import json
 
-HOST = '127.0.0.1'
-PORT = 12345
+root = Tk()
+root.title('Pizza order')
+root.geometry('300x300')
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b'I want to order a pizza, please.')
-    data = s.recv(1024)
+Last_name_label = Label(root, text = 'Last Name:')
+Last_name_label.pack()
+Last_name_entry = Entry(root)
+Last_name_entry.pack()
 
-print(f'Received {data!r}')
+Phone_number_label = Label(root, text = 'Phone Number:')
+Phone_number_label.pack()
+Phone_number_entry = Entry(root)
+Phone_number_entry.pack()
+
+def Order_pizza() :
+    Last_name = Last_name_entry.get()
+    Phone_number = Phone_number_entry.get()
+
+    New_order = {'Last_name' : Last_name, 'Phone_number': Phone_number}
+    
+    Order_json_string = json.dumps(New_order)
+
+    HOST = '127.0.0.1'
+    PORT = 12345
+
+    try: 
+
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((HOST, PORT))
+            s.sendall(Order_json_string.encode('utf-8'))
+            data = s.recv(1024)
+        
+        messagebox.showinfo('Success', 'Pizza ordered correctly')
+    except Exception as e:
+        messagebox.showerror('Error', f'Order could not be processed, try again, {e}')
+
+        print(f'Received {data!r}')
+
+
+
+Order_button = Button(root, text='Order', command=Order_pizza)
+Order_button.pack()
+
+root.mainloop()
+
+
+
+# Refactor code to send a JSON data with an order
+# Include a Tkinter GUI with a form that asks for
+# a name, phone number, pizza size and ingredients
+# the ingredients will be selected using a 
+# select menu where they can check a box 
+# if they want that ingredient in their pizza
+# an option for delivery and pickup will release
+# a second form to enter the address.
+# add validation to the tkinter form.
+
--- pizza_server.py
+++ pizza_server.py
@@ -1,4 +1,5 @@
-import socket;
+import socket
+import json
 
 HOST = '127.0.0.1'
 PORT = 12345
@@ -13,4 +14,15 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             data = conn.recv(1024)
             if not data:
                 break
-            conn.sendall(data)
+            received_string = data.decode('utf-8')
+            parsed_order = json.loads(received_string)
+            print (f'Order data:{parsed_order}')
+            conn.sendall('What should I send here?')
+            
+
+#Refactor server to send back a tkinter confirmation
+# page containing the JSON info of the order
+# and the estimated time for pick up which will be 
+# calculated by the number of ingredients picked
+# if delivery was selected, 25 minutes will be added
+# to the calculated time
